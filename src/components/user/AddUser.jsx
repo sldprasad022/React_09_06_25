ab82218f45bedfa41a00a14225635a7326b81f5b
sldprasad022 <sldprasad022@gmail.com>
2025-06-12 11:22:37 +0530

    second commit

--- src/components/user/AddUser.jsx
+++ src/components/user/AddUser.jsx
@@ -4,7 +4,8 @@ const AddUser = () => {
   return (
   <>
     <div className='text-2xl text-center'>AddUser</div>
-    <h1>First Name</h1>  
+    <h1>First Name</h1> 
+    <h2>last name</h2> 
   </>
     
   )
